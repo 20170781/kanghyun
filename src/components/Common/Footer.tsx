34461e3198d82c3d463cf21917043d756389dc1d
20170781 <lkh981107@naver.com>
2021-08-13 15:40:08 +0900

    make Introduction, Footer on Index page

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,24 @@
+// 페이지 하단 Footer
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+const FooterWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const Footer: FunctionComponent = function () {
+    return (
+      <FooterWrapper>
+        Thank You for Visiting My Blog, Have a Good Day 😆
+        <br />© 2021 Developer KangHyun, Powered By Gatsby.
+      </FooterWrapper>
+    );
+  };
+
+export default Footer;
