1ef19ae5a9d478c64d483a39ca7233a4afc21e9e
20170781 <lkh981107@naver.com>
2021-08-14 18:20:10 +0900

    add system for Markdown files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
+import { graphql } from 'gatsby';
 import GlobalStyle from 'components/Common/GlobalStyle';
 import Introduction from 'components/Main/Introduction';
 import Footer from 'components/Common/Footer';
 import CategoryList from 'components/Main/CategoryList';
-import PostList from 'components/Main/PostList';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-`
+import PostList, {PostType} from 'components/Main/PostList';
 
 const CATEGORY_LIST = {
   All: 5,
@@ -18,16 +13,64 @@ const CATEGORY_LIST = {
   TypeScript: 2,
 };
 
-const IndexPage: FunctionComponent = function () {
+interface IndexPageProps {
+  data: {
+    allMarkdownRemark: {
+      edges: PostType[];
+    };
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="JavaScript" categoryList={CATEGORY_LIST} />
-      <PostList />
+      <PostList posts={edges}/>
       <Footer />
     </Container>
   );
 };
 
 export default IndexPage;
+
+export const queryPostList = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            categories
+            date
+            slug
+            summary
+            title
+            thumbnail {
+              childImageSharp {
+                fluid(
+                  maxWidth: 768
+                  maxHeight: 200
+                  fit: INSIDE
+                  quality: 100
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
