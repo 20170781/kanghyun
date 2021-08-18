c12372f63376b3aa360a4d49d72d063b2583a95c
20170781 <lkh981107@naver.com>
2021-08-19 01:47:37 +0900

    Restore repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,14 @@ import queryString, { ParsedQuery } from 'query-string';
 
 import Introduction from 'components/Main/Introduction';
 import { ProfileImageProps } from 'components/Main/ProfileImage';
+import CategoryList, { CategoryListProps } from 'components/Main/CategoryList';
+import PostList, { PostType } from 'components/Main/PostList';
 import Template from 'components/Common/Template';
 
 interface IndexPageProps {
+  location: {
+    search: string;
+  };
   data: {
     site: {
       siteMetadata: {
@@ -14,6 +19,9 @@ interface IndexPageProps {
         description: string;
         siteUrl: string;
       };
+    };
+    allMarkdownRemark: {
+      edges: PostType[];
     };
     file: {
       publicURL: string;
@@ -25,16 +33,50 @@ interface IndexPageProps {
 }
 
 const IndexPage: FunctionComponent<IndexPageProps> = ({
+  location: { search },
   data: {
     site: {
       siteMetadata: { title, description, siteUrl },
     },
+    allMarkdownRemark: { edges },
     file: {
       publicURL,
       childImageSharp: { fluid },
     },
   },
 }: any) => {
+  const parsed: ParsedQuery<string> = queryString.parse(search);
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category;
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'], // acc
+          {
+            node: {
+              frontmatter: { categories }, // cur
+            },
+          }: PostType,
+        ) => {
+          categories.forEach((category) => {
+            if (category in list) {
+              list[category] += 1;
+            } else {
+              list[category] = 1;
+            }
+          });
+          list.All += 1;
+          return list;
+        },
+        { All: 0 },
+      ),
+    [],
+  );
+
   return (
     <Template
       title={title}
@@ -43,14 +85,19 @@ const IndexPage: FunctionComponent<IndexPageProps> = ({
       image={publicURL}
     >
       <Introduction profileImage={fluid} />
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList posts={edges} selectedCategory={selectedCategory} />
     </Template>
   );
 };
 
 export default IndexPage;
 
-export const queryIndex = graphql`
-  query queryIndex {
+export const queryPostList = graphql`
+  query queryPostList {
     site {
       siteMetadata {
         title
@@ -58,7 +105,38 @@ export const queryIndex = graphql`
         siteUrl
       }
     }
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            categories
+            date(formatString: "YYYY.MM.DD.")
+            summary
+            title
+            thumbnail {
+              childImageSharp {
+                fluid(
+                  maxWidth: 768
+                  maxHeight: 200
+                  fit: INSIDE
+                  quality: 100
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     file(name: { eq: "profile-image" }) {
+      publicURL
       childImageSharp {
         fluid(maxWidth: 120, maxHeight: 120, fit: INSIDE, quality: 100) {
           ...GatsbyImageSharpFluid_withWebp
--- src/pages/portfolio.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PortfolioPage = (props) => <h1>Portfolio Page</h1>;
-
-export default PortfolioPage;
