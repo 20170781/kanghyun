03bbef5a49d81f2ad7d90c41bc7122cab61694ec
20170781 <lkh981107@naver.com>
2021-08-19 09:59:36 +0900

    Rebuilding Blog Constructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
-import queryString, { ParsedQuery } from 'query-string';
 
 import Introduction from 'components/Main/Introduction';
 import { ProfileImageProps } from 'components/Main/ProfileImage';
-import CategoryList, { CategoryListProps } from 'components/Main/CategoryList';
-import PostList, { PostType } from 'components/Main/PostList';
 import Template from 'components/Common/Template';
 
 interface IndexPageProps {
-  location: {
-    search: string;
-  };
   data: {
     site: {
       siteMetadata: {
@@ -19,9 +13,6 @@ interface IndexPageProps {
         description: string;
         siteUrl: string;
       };
-    };
-    allMarkdownRemark: {
-      edges: PostType[];
     };
     file: {
       publicURL: string;
@@ -33,50 +24,16 @@ interface IndexPageProps {
 }
 
 const IndexPage: FunctionComponent<IndexPageProps> = ({
-  location: { search },
   data: {
     site: {
       siteMetadata: { title, description, siteUrl },
     },
-    allMarkdownRemark: { edges },
     file: {
       publicURL,
       childImageSharp: { fluid },
     },
   },
 }: any) => {
-  const parsed: ParsedQuery<string> = queryString.parse(search);
-  const selectedCategory: string =
-    typeof parsed.category !== 'string' || !parsed.category
-      ? 'All'
-      : parsed.category;
-
-  const categoryList = useMemo(
-    () =>
-      edges.reduce(
-        (
-          list: CategoryListProps['categoryList'], // acc
-          {
-            node: {
-              frontmatter: { categories }, // cur
-            },
-          }: PostType,
-        ) => {
-          categories.forEach((category) => {
-            if (category in list) {
-              list[category] += 1;
-            } else {
-              list[category] = 1;
-            }
-          });
-          list.All += 1;
-          return list;
-        },
-        { All: 0 },
-      ),
-    [],
-  );
-
   return (
     <Template
       title={title}
@@ -85,54 +42,19 @@ const IndexPage: FunctionComponent<IndexPageProps> = ({
       image={publicURL}
     >
       <Introduction profileImage={fluid} />
-      <CategoryList
-        selectedCategory={selectedCategory}
-        categoryList={categoryList}
-      />
-      <PostList posts={edges} selectedCategory={selectedCategory} />
     </Template>
   );
 };
 
 export default IndexPage;
 
-export const queryPostList = graphql`
-  query queryPostList {
+export const queryIndex = graphql`
+  query queryIndex {
     site {
       siteMetadata {
         title
         description
         siteUrl
-      }
-    }
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
-    ) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            categories
-            date(formatString: "YYYY.MM.DD.")
-            summary
-            title
-            thumbnail {
-              childImageSharp {
-                fluid(
-                  maxWidth: 768
-                  maxHeight: 200
-                  fit: INSIDE
-                  quality: 100
-                ) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-        }
       }
     }
     file(name: { eq: "profile-image" }) {
